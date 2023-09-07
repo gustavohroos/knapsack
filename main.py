88f7a17eacd96bc80c5502b69efb2b9cbf80d228
Gustavo Roos <gustavohroos@gmail.com>
2023-09-07 17:32:36 -0300

    change iteration logic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+from itertools import product
+import time
 
 with open('data/p01/p01_c.txt') as f:
     MAX_WEIGHT = int(f.readline())
@@ -22,31 +24,25 @@ def bruteforce(weights, values, max_weight):
     n = len(weights)
     max_value = 0
     final_weight = 0
+    best_items = np.zeros(n, dtype=int)
+    start = time.time()
 
-    best_items = np.zeros(n, dtype=int)  # Initialize an array to store the selected items
-
-    for i in range(2**n):
-        weight = 0
-        value = 0
-        items = np.zeros(n, dtype=int)  # Initialize an array to represent selected items in binary
-
-        for j in range(n):
-            if (i >> j) & 1:
-                weight += weights[j]
-                value += values[j]
-                items[j] = 1  # Mark item as selected
+    for combination in product([0, 1], repeat=n):
+        weight = np.sum(np.array(combination) * weights)
+        value = np.sum(np.array(combination) * values)
 
         if weight <= max_weight and value > max_value:
             final_weight = weight
             max_value = value
-            best_items = items.copy()  # Update the best_items array
+            best_items = np.array(combination)
     
-    return max_value, final_weight, best_items
+    return time.time() - start, max_value, final_weight, best_items
 
-max_value, final_weight, best_items = bruteforce(weights, values, MAX_WEIGHT)
+total_time, max_value, final_weight, best_items = bruteforce(weights, values, MAX_WEIGHT)
 
 print('Max value: ', max_value)
 print('Final weight: ', final_weight)
 print('Best items: ', best_items)
+print('Total time: ', total_time)
 
 print('Equals solution? ', (solution == best_items).all())
