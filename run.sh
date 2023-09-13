2eb000d9ee2dd3ab6d5106afcf04758ecec8e375
Gustavo Roos <gustavohroos@gmail.com>
2023-09-13 14:35:46 -0300

    change to run the low dimensional files

--- run.sh
+++ run.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
 
 files="
-knapPI_1_10000_1000_1 
-knapPI_1_1000_1000_1 
-knapPI_1_100_1000_1 
-knapPI_1_2000_1000_1 
-knapPI_1_200_1000_1 
-knapPI_1_5000_1000_1 
-knapPI_1_500_1000_1 
-knapPI_2_10000_1000_1 
-knapPI_2_1000_1000_1 
-knapPI_2_100_1000_1 
-knapPI_2_2000_1000_1 
-knapPI_2_200_1000_1 
-knapPI_2_5000_1000_1 
-knapPI_2_500_1000_1 
-knapPI_3_10000_1000_1 
-knapPI_3_1000_1000_1 
-knapPI_3_100_1000_1 
-knapPI_3_2000_1000_1 
-knapPI_3_200_1000_1 
-knapPI_3_5000_1000_1 
-knapPI_3_500_1000_1 
+kp_10_269
+kp_10_60
+kp_20_878
+kp_20_879
+kp_23_10000
+kp_24_6404180
+kp_4_11
+kp_4_20
+kp_5_80
+kp_7_50
 "
 
 algorithms="
+bruteforce
 greedy 
-dynamic 
-branch_and_bound 
+dynamic
 "
 
 for i in {1..100}; do
